7e894b63877e9ef276ca1688c3e2dffcfab699e4
jjsh0208 <jjsh0208@gmail.com>
2024-06-13 09:09:25 +0900

    modal 기능까지 구현/ 트래픽 관리 중요

---
+++ src/main/java/com/example/youtube_player/config/CashConfig.java
@@ -0,0 +1,10 @@
+package com.example.youtube_player.config;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CashConfig {
+}
--- src/main/java/com/example/youtube_player/service/YoutubeService.java
+++ src/main/java/com/example/youtube_player/service/YoutubeService.java
@@ -6,7 +6,9 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,26 +16,30 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @Service
 public class YoutubeService {
 
-    //@Value 어노테이션을 사용하여 application.yml에서 정의한 YouTube API 키를 주입 받음
+
+    //@Value 어노테이션을 사용하여 application.properties 에서 설정한 key 가져와서 apikey에 주입
     @Value("${youtube.api.key}")
     private String apiKey;
 
-    public HashMap<String,String> searchVideo(String query) throws IOException {
-        // JSON 데이터를 처리하기 위한 JsonFactory 객체 생성
-        JsonFactory jsonFactory = new JacksonFactory();
+    // JSON 데이터를 처리하기 위한 JsonFactory 객체 생성
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-        // YouTube 객체를 빌드하여 API에 접근할 수 있는 YouTube 클라이언트 생성
-        YouTube youtube = new YouTube.Builder(
-                new com.google.api.client.http.javanet.NetHttpTransport(),
-                jsonFactory,
-                request -> {})
-                .build();
+    // YouTube 객체를 빌드하여 API에 접근할 수 있는 YouTube 클라이언트 생성
+    private static final YouTube YOUTUBE = new YouTube.Builder(
+            new com.google.api.client.http.javanet.NetHttpTransport(),
+            JSON_FACTORY,
+            request -> {}).setApplicationName("youtube-caching-example").build();
+
+    @Cacheable("youtubeSearchCache")
+    public HashMap<String,String> searchVideo(String query) throws IOException {
+        log.info("test"); //캐싱 확인용
 
         // YouTube Search API를 사용하여 동영상 검색을 위한 요청 객체 생성
-        YouTube.Search.List search = youtube.search().list(Collections.singletonList("id,snippet"));
+        YouTube.Search.List search = YOUTUBE.search().list(Collections.singletonList("id,snippet"));
 
         // API 키 설정
         search.setKey(apiKey);
@@ -41,8 +47,14 @@ public class YoutubeService {
         // 검색어 설정
         search.setQ(query);
 
-        //검색 결과 개수 10개로 설정
-        search.setMaxResults(1L);
+        //조회되는 타입 (채널,리스트 제외)
+        search.setType(Collections.singletonList("video"));
+        
+        //카테고리 id 10은 음악 (음악 동영상만 가져온다.)
+        search.setVideoCategoryId("10");
+        
+        //검색 결과 개수 3개로 설정
+        search.setMaxResults(3L);
 
         // 검색 요청 실행 및 응답 받아오기
         SearchListResponse searchResponse = search.execute();
@@ -51,18 +63,19 @@ public class YoutubeService {
         List<SearchResult> searchResultList = searchResponse.getItems();
 
         //뷰로 전달할 해시맵
-        HashMap<String,String> youtubeInfo = new HashMap<>();
+        HashMap<String,String> youtubeInfo = new HashMap<>(); //제목 : 동영상 url
 
-        if (searchResultList != null && searchResultList.size() > 0) {
-            //검색 결과 중 첫 번째 동영상 정보 가져오기
-            for (SearchResult s : searchResultList){
-                String videoTitle = s.getSnippet().getTitle();
-                String videoId = s.getId().getVideoId();
-                //채널 자체를 가져오면 url VideoId가 null 로 반환된다. 건너뛰기
-                if (videoId == null) continue;
-                youtubeInfo.put(videoTitle,videoId);
+            if (searchResultList != null && searchResultList.size() > 0) {
+                //검색 결과 중 첫 번째 동영상 정보 가져오기
+                for (SearchResult s : searchResultList) {
+                    String videoTitle = s.getSnippet().getTitle();
+                    String videoId = s.getId().getVideoId();
+                    //채널 자체를 가져오면 url VideoId가 null 로 반환된다. 건너뛰기
+                    if (videoId == null) continue;
+                    youtubeInfo.put(videoTitle, videoId);
+                }
             }
-        }
+
         return youtubeInfo;
     }
 }
